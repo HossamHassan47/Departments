2117a8230610963db82c19fcaa9434647b310c75
HossamHassan47 <hossam.hassan@outlook.com>
2024-07-20 16:14:32 +0300

    prepare Department view model

--- Departments/Controllers/DepartmentsController.cs
+++ Departments/Controllers/DepartmentsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Departments.Controllers
 {
-    public class DepartmentsController (ApplicationDbContext dbContext) : Controller
+    public class DepartmentsController(ApplicationDbContext dbContext) : Controller
     {
         private readonly ApplicationDbContext _dbContext = dbContext;
 
@@ -13,6 +13,50 @@ namespace Departments.Controllers
 
 
             return View(departments);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            Department? department = _dbContext.Departments.Find(id);
+
+            if (id == null || id == 0 || department == null)
+            {
+                return NotFound();
+            }
+
+            // Get Sub Departments
+            List<Department> subDepartments = this.FindSubDepartments(id).ToList();
+
+            // Get Parent Departments
+            List<Department> parentDepartments = this.FindParentDepartments(id).ToList();
+
+            // Prepare View Model
+            DepartmentDto departmentDto = new DepartmentDto
+            {
+                Department = department,
+                SubDepartments = subDepartments,
+                ParentDepartments = parentDepartments
+            };
+
+            return View(departmentDto);
+        }
+
+        private IQueryable<Department> FindSubDepartments(int? id)
+        {
+            return _dbContext.Departments.Where(
+                descendent => descendent.Path.GetAncestor(1) == _dbContext.Departments.Single(ancestor => ancestor.Id == id).Path);
+        }
+
+        private IQueryable<Department> FindParentDepartments(int? id)
+        {
+            return _dbContext.Departments.Where(
+                ancestor => _dbContext.Departments
+                    .Single(
+                        descendent =>
+                            descendent.Id == id
+                            && ancestor.Id != descendent.Id)
+                    .Path.IsDescendantOf(ancestor.Path))
+                .OrderByDescending(ancestor => ancestor.Path.GetLevel());
         }
     }
 }
---
+++ Departments/Models/DepartmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Departments.Models
+{
+    public class DepartmentDto
+    {
+        public Department Department { get; set; }
+
+        public List<Department>? SubDepartments { get; set; }
+
+        public List<Department>? ParentDepartments { get; set; }
+    }
+}
