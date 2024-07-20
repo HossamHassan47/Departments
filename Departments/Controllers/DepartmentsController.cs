03bfecf515a33b5b3bd1f41ef93cf83ce8079f54
HossamHassan47 <hossam.hassan@outlook.com>
2024-07-20 15:22:21 +0300

    view departments list

--- Departments/Controllers/DepartmentsController.cs
+++ Departments/Controllers/DepartmentsController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Departments.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Departments.Controllers
 {
-    public class DepartmentsController : Controller
+    public class DepartmentsController (ApplicationDbContext dbContext) : Controller
     {
+        private readonly ApplicationDbContext _dbContext = dbContext;
+
         public IActionResult Index()
         {
-            return View();
+            List<Department> departments = _dbContext.Departments.ToList();
+
+
+            return View(departments);
         }
     }
 }
