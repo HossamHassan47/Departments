6c3d88c7dd5f523472c6ba8b4420f182bbab54ce
BlueHossam <hossam@blue-developments.team>
2024-07-20 13:20:09 +0300

    Add project files.

---
+++ Departments/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Departments.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
