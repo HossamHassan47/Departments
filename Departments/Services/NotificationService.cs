abc94905e7796696ff7359b5c110614997ab7c02
HossamHassan47 <hossam.hassan@outlook.com>
2024-07-20 19:10:05 +0300

    Send Notification Service

--- Departments/Services/NotificationService.cs
+++ Departments/Services/NotificationService.cs
@@ -1,5 +1,6 @@
 ﻿
 using Departments.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Departments.Services
 {
@@ -29,32 +30,41 @@ namespace Departments.Services
         Task HandleNotificationsAsync();
     }
 
-    //public class NotificationHandler(ApplicationDbContext dbContext) : INotificationHandler
-    //{
-    //    private readonly ApplicationDbContext _dbContext = dbContext;
-
-    //    public async Task HandleNotificationsAsync()
-    //    {
-    //        // Check for scheduled reminders
-    //        var reminders = _dbContext.Reminders.ToList();
-
-    //        foreach (var reminder in reminders)
-    //        {
-    //            Console.WriteLine(string.Format("Send Reminder for Title: {0}, Time: {1}", reminder.Title, reminder.ReminderTime));
-
-    //            // Logic to send email notifications.
-    //        }
-    //    }
-    //}
-
-    public class NotificationHandler: INotificationHandler
+    public class NotificationHandler : INotificationHandler
     {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public NotificationHandler(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
         public async Task HandleNotificationsAsync()
         {
-                Console.WriteLine("Send Reminders...");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Send Reminders Started at " + DateTime.Now);
+
+            using IServiceScope scope = _scopeFactory.CreateScope();
+            ApplicationDbContext _dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+            // Check for scheduled reminders
+            var _now = DateTime.Now;
+
+            var reminders = _dbContext.Reminders
+                .Where(r =>
+                    r.ReminderTime.Date == _now.Date && // Same Date
+                    r.ReminderTime.Hour == _now.Hour && // Same Hour
+                    r.ReminderTime.Minute == _now.Minute // Same Minute
+                    ).ToList();
+
+            foreach (var reminder in reminders)
+            {
+                Console.WriteLine(string.Format("Reminder sent for Title: {0}, Time: {1}", reminder.Title, reminder.ReminderTime));
 
                 // Logic to send email notifications.
-            
+            }
+
+            Console.WriteLine("Send Reminders Ended");
         }
     }
 }
